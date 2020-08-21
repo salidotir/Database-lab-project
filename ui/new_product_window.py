690fc84e2fa3439cb15cd69c04e9ea826809ef96
Sara Limooee <35997721+salidotir@users.noreply.github.com>
2020-08-22 00:01:47 +0430

    Implement new product add command

--- ui/new_product_window.py
+++ ui/new_product_window.py
@@ -1,13 +1,12 @@
-from sqlalchemy import create_engine
+from Schema import PRODUCT, db_session
 from tkinter import *
 
 
-def run_query_on_db():
+def run_query_on_db(**kwargs):
+    p = PRODUCT(**kwargs)
+    db_session.add(p)
+    db_session.commit()
     print("Product inserted.")
-
-    #
-    # To Be Completed
-    #
 
 
 def create_new_product_window():
@@ -19,22 +18,18 @@ def create_new_product_window():
 
     main_label = Label(root, text='New product').place(x=220, y=50)
 
-    pid_label = Label(root, text="pid").place(x=160, y=100)
-    global pid
-    pid = StringVar()
-    pid_text_box = Entry(root, width=20, textvariable=pid).place(x=215, y=100)
+    pname_label = Label(root, text="pName").place(x=160, y=100)
+    pname = StringVar(master=root)
+    pname_text_box = Entry(root, width=20, textvariable=pname).place(x=215, y=100)
 
-    pname_label = Label(root, text="pname").place(x=160, y=130)
-    global pname
-    pname = StringVar()
-    pname_text_box = Entry(root, width=20, textvariable=pname).place(x=215, y=130)
+    price_label = Label(root, text="price").place(x=160, y=130)
+    p_price = StringVar(master=root)
+    price_text_box = Entry(root, width=20, textvariable=p_price).place(x=215, y=130)
 
-    price_label = Label(root, text="price").place(x=160, y=160)
-    global price
-    price = StringVar()
-    price_text_box = Entry(root, width=20, textvariable=price).place(x=215, y=160)
-
-    btn_add = Button(root, text="Add", command=run_query_on_db).place(x=240, y=200)
+    btn_add = Button(root, text="Add", command=lambda: run_query_on_db(
+        pName=pname.get(),
+        price=p_price.get()
+    )).place(x=240, y=160)
 
     # ~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.
 
